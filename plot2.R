9c23ad17058fee4c66d7eed76e84d0303ffdc0a7
tcw176 <tyronewong@gmail.com>
2015-03-21 18:57:55 +0800

    Plot 2 R update

--- plot2.R
+++ plot2.R
@@ -14,7 +14,7 @@ BaltimorePollution <- filter(NEI, fips == "24510") %>%
       summarise(EmissionTotal = sum(Emissions))
 
 plot(BaltimorePollution$year, BaltimorePollution$EmissionTotal, xlab ="Year", ylab = "Emission Total")
-model <- lm(EmissionTotal ~ year, SBPol)
+model <- lm(EmissionTotal ~ year, BaltimorePollution)
 abline(model, lw = 2)
 title(main = "Total PM 2.5 Emissions from All Sources for the City of Baltimore")
 legend("topright", legend = c("Emission Total", "Line of Best Fit"), pch = c(1,NA), lwd = c(NA,2))
